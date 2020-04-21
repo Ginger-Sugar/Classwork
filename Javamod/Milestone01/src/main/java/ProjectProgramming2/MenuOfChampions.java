44cbf261faa0beed537554738a4d847731532ae1
shanat24 <staylor@soka.edu>
2020-04-21 19:25:40 -0400

    New exercises

---
+++ Javamod/Milestone01/src/main/java/ProjectProgramming2/MenuOfChampions.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectProgramming2;
+
+/**
+ *
+ * @author shana
+ */
+public class MenuOfChampions {
+
+    public static void main(String[] args) {
+        
+        String food1, food2, food3;
+        float price1, price2, price3;
+
+        price1 = 5;
+        food1 = "Milk Tea";
+        price2 = 8;
+        food2 = "Ice Cream Toast";
+        price3 = 10;
+        food3 = "Shaved Ice";
+        
+                
+                
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+        System.out.println("                Welcome to Sweet T's                    ");
+        System.out.println("                    Today's Menu:                            ");
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+
+        System.out.println(food1 + "      " +"\t" + price1);
+        System.out.println(food2 + "    " +"\t" + price2);
+        System.out.println(food3 +"      " + "\t" + price3);
+
+    }
+
+}
