7b753ff2f34c2607f635957daf33ebf7c71de55a
shanat24 <staylor@soka.edu>
2020-04-21 20:07:10 -0400

    New Exercises

---
+++ Javamod/Milestone01/src/main/java/ProjectProgramming2/BestAdderEver.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectProgramming2;
+
+/**
+ *
+ * @author shana
+ */
+public class BestAdderEver {
+    
+    
+    public static void main (String [] args) {
+        
+        int cats = 1, dogs = 2, vampires = 3;
+        
+        int num1 = cats + dogs + vampires;
+        
+        System.out.println(cats + " + " + dogs + " + " + vampires + " = " + num1);
+        
+        
+                
+        
+        
+        
+        
+        
+        
+        
+}
+    
+}
