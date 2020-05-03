02ac970a0c3677274ee1cc0f60657753166b6000
shanat24 <staylor@soka.edu>
2020-05-03 09:10:42 -0400

    New exercises

--- Javamod/Milestone01/src/main/java/PracticeProgramming7/practice.java
+++ Javamod/Milestone01/src/main/java/PracticeProgramming7/practice.java
@@ -13,26 +13,5 @@ import java.util.Scanner;
  * @author shana
  */
 public class practice {
-public static void n_random(int count, int finalSum)
-{
-    Random r = new Random();
-    int numbers[] = new int[count];
-    int sum = 0;
-    for (int i = 0; i < count - 1; i++)
-    {
-        numbers[i] = r.nextInt((finalSum - sum) / 2) + 1;
-        sum += numbers[i];
-    }
-    numbers[count - 1] = finalSum - sum;
 
-    StringBuilder numbersString = new StringBuilder("Random number list: ");
-    for (int i = 0; i < count; i++)
-        numbersString.append(numbers[i] + " ");
-    System.out.println(numbersString);
-}
-
-public static void main(String[] args)
-{
-    n_random(9, 250);
-}
 }
